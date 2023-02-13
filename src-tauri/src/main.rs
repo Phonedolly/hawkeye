5b0e32f0c46a7f18cdb1083c70efe6ec0c5ea040
Phonedolly <Phonedolly@users.noreply.github.com>
2023-02-13 23:31:54 +0900

    add tray menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,85 @@
     windows_subsystem = "windows"
 )]
 
+use std::fs;
+use std::path::PathBuf;
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{Manager, SystemTrayEvent};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// fn read_config()->PathBuf{
+//     tauri::api::path::parse(config, package_info, env, path)
+// }
+
 fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let show = CustomMenuItem::new("show".to_string(), "Hide");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(show)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::DoubleClick {
+                tray_id,
+                position,
+                size,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+
+                if window.is_visible().unwrap() == true {
+                    window.set_focus().unwrap();
+                } else {
+                    window.show().unwrap();
+                }
+            }
+            SystemTrayEvent::MenuItemClick { tray_id, id, .. } => {
+                let item_handle = app.tray_handle().get_item(&id);
+                match id.as_str() {
+                    "show" => {
+                        let window = app.get_window("main").unwrap();
+                        if window.is_visible().unwrap() == true {
+                            item_handle.set_title("Show").unwrap();
+                            window.hide().unwrap();
+                        } else {
+                            item_handle.set_title("Hide").unwrap();
+                            window.show().unwrap();
+                        }
+                    }
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        // .run(tauri::generate_context!())
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| match event {
+            tauri::RunEvent::WindowEvent { label, event, .. } => match event {
+                tauri::WindowEvent::CloseRequested { api, .. } => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                    api.prevent_close();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
 }
